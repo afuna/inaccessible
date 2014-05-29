7ebd33ad2d0b3479fb9b092786940abd7cd19831
Afuna <fu@dreamwidth.org>
2014-05-28 20:17:08 -0700

    Highlight the lines that we want to be changed in the editor

--- js/codeEditor.js
+++ js/codeEditor.js
@@ -1,4 +1,26 @@
 function CodeEditor(textareaId, game) {
+    var highlightedLines = [];
+    var modifiedCode = [];
+
+    function preprocess(code) {
+        var lines = code.split("\n");
+        for (var i in lines) {
+            var line = lines[i];
+            if (line.startsWith("!")) {
+                highlightedLines.push(i);
+                line = line.replace("!", " ");
+            }
+            modifiedCode.push(line);
+        }
+        return modifiedCode.join("\n");
+    }
+
+    function highlightLines(editor) {
+        for (var i in highlightedLines) {
+            var lineNumber = parseInt(highlightedLines[i], 10);
+            editor.addLineClass(lineNumber, 'wrap', 'editable');
+        }
+    }
 
     this.initialize = function() {
         this.editor = CodeMirror.fromTextArea(document.getElementById(textareaId), {
@@ -10,9 +32,15 @@ function CodeEditor(textareaId, game) {
 
         this.editor.on("changes", function(editor) {
             game.update(editor.getValue());
-        })
+        });
+
         game.update(this.editor.getValue());
-    }
+    };
+
+    this.loadCode = function(code) {
+        this.editor.setValue(preprocess(code));
+        highlightLines(this.editor);
+    };
 
     this.initialize();
 };
